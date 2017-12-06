d5256fec2f8ec88ee03d0149fe494755013f5372
Matt Slevin <matt.slevin@ucdconnect.ie>
2017-12-06 17:19:28 +0000

    Updated file merge
    -Sub directories now working
    -Cleaned up code (better comments, varible names)
    -Using dicts as opposed to lists, makes code more readable
    -Pickels half set up for storing data

--- file_merge.py
+++ file_merge.py
@@ -3,121 +3,110 @@
 Created on Tue Dec  5 09:48:26 2017
 
 @author: matt.slevin
+
+Functions to read in raw ascii datafiles, it then stores the files into a
+one dictionary.  
 """
+
+"""
+TODO:
+    Add support for different tilts. 
+    the challange is that we dont nessarily know how many tilts there are for
+    a particular antenna and not every tilt will have an az_co and az_cross. It
+    could be solved by have Tilt dictionary. 
+
+TODO:
+    Add support for different ports. We dont know how many ports each antenna 
+    will have the same tilts for every port.
+
+TODO:
+    Save the final dictionary as a pickel as a master file. Half set up
+
+"""
+
 import pandas as pd
-import os 
-import numpy as np
-#Script one, for organising data storage. Raw input is the csv files.  
+import os
+import pickle
 
-#
-#   Import all our data files to pandas
-#
-
+#function to read in an ascii test file and generate a dictionary. Which 
+#contains the amplitude and phase tables for a particular measurment.  
 def read_to_panda(f_name):
     """
     Function to read in ascii data files from Midas. 
-    amplitude, phase = read_to_panda(f_name)
+    P1= read_to_panda(f_name)
     """
     
-    
-    #Read in the file 
+    #Read in the file as a string
     f=open(f_name, 'r')
     string=f.read()
+    f.close()
     
-    #Split the file based on the frequency,
-    a =string.split("Frequency") 
+    #Split the file based on the frequency. Using the word 'frequency' as delimiter 
+    data_per_frequency =string.split("Frequency") 
     
     #Initalise amplitude and phase dataframes
     df_amplitude=pd.DataFrame()
     df_phase=pd.DataFrame()
     
-    angle=list()
+    #Initalise lists for each column
     amplitude=list()
     phase=list()
+
+    #Loop to iterate through each frequency. Capturing each column appropriatly.      
+    for j in range(1,len(data_per_frequency)-1):
+        
+        frequency_val= data_per_frequency[j].split("\t")[1] # Capture the frequency value
     
-    for j in range(1,len(a)-1):
-        frequency= a[j].split("\t")[1] # Capture the frequency value
+        #Get rid of the header file
+        data_one_frequency=data_per_frequency[j].split("\t\n\t\t\t\n") 
     
-        b=a[j].split("\t\n\t\t\t\n") #Get rid of the header file
-    
-        # b[1] is now just our angle amplitude phase 
-        angle.clear()
+        # data_per_f is now just our angle amplitude phase 
         phase.clear()
         amplitude.clear()
-    
-        c=b[1].split("\n")
-    
-        for i in range(0,len(c)-1):
-            d,e,f,g=c[i].split("\t")
-    
-            angle.append(d)
-            amplitude.append(e)
-            phase.append(f)
-    
-        #Convert a list to a numpy array 
-        angle_np = np.asarray(angle, dtype=np.float32)
-        amplitude_np = np.asarray((amplitude), dtype=np.float32)
-        phase_np = np.asarray(phase, dtype=np.float32)
-    
-        #
-        # Put into a panda
-        #
-    
-        df_amplitude[frequency]= amplitude_np
-        df_phase[frequency]= phase_np
         
-    return df_amplitude,df_phase
+        #Seperate each row. 
+        data_per_row=data_one_frequency[1].split("\n")
+    
+        #loop to sort each row into approiate list
+        for i in range(0,len(data_per_row)-1):
+            _, amplitude_val ,phase_val ,_ =data_per_row[i].split("\t")
+            
+            #Apend values onto end of list
+            amplitude.append(amplitude_val)
+            phase.append(phase_val)
+    
+        #put array into a panda    
+        df_amplitude[frequency_val]= amplitude
+        df_phase[frequency_val]= phase
+        
+    #create a dict with amp and phase tables as data frames
+    data_frame_amp_phase={"amplitude":df_amplitude,"phase":df_phase}
+        
+    return data_frame_amp_phase
 
-#The working directory is,
-#f_dir="C:\Users\matt.slevin\Documents\data acquisition_systme\RAW_DATA\RAW_DATA"
-
-# TODO: Set up automatic change to our working dir
-#os.chdir("Users\matt.slevin\Documents\data acquisition_systme\RAW_DATA\RAW_DATA")
-
-def read_in_port_data():    
-    #Port 1
+#Function to read in data for one port
+def read_in_port_data():
+    #File names    
     fname_az_copolar="AW3023_V4_R1_AZ_CO_P1_T0.txt"
     fname_az_cross="AW3023_V4_R1_AZ_CR_P1_T0.txt"
     fname_el_copolar="AW3023_V4_R1_EL_CO_P1_T0.txt"
 
-    az_co=read_to_panda(fname_az_copolar)
-    az_cross=read_to_panda(fname_az_cross)
-    el_co=read_to_panda(fname_el_copolar)
+    #Setting up path to subdir
+    sub_dir = "\\RAW_DATA\\RAW_DATA\\"
+    path=os.getcwd()+sub_dir
 
-    p1_data=[az_co,az_cross,el_co]
-    #break line by line
-    #break into tabs
+    #Read in each file
+    az_co=read_to_panda(path+fname_az_copolar)
+    az_cross=read_to_panda(path+fname_az_cross)
+    el_co=read_to_panda(path+fname_el_copolar)
+
+    #Put all measurments into a dict
+    p1_data={"az_co":az_co,"az_cross":az_cross,"el_co":el_co}
+
+    #dump the data
+    #pickle.dump( p1_data, open( "p1_data.p", "wb" ) )
 
     return p1_data
-
-
-
-
-#Read in csv file as a string
-#Put into panda dataframes, or numpy
-
-#
-#   Formating of our panda files (get rid of our unessary data)
-#
-
-#Save panda as a json
-
-#For testing purposes read the json f
-
-
-#
-# Micilanious 
-#
-
-"""
-Functions which could be usefull
-
-os.getcwd() - get the current working directory
-os.chdir("f_dir") - change the working dir to f_dir
-os.listdir("f_dir") - get a list of files in a specific dir
-os.remove("f_name") - delete f_name from the directory
-os.mkdir("f_dir") - Create a subdirectory f_dir
-"""
 
 
 
